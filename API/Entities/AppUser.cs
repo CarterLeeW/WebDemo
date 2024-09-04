25d88d75711a52b53d2c388342b318640e2cf054
CarterLeeW <103856985+CarterLeeW@users.noreply.github.com>
2024-09-04 12:02:45 -0400

    first commit

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace API.Entities;
+
+public class AppUser
+{
+    public int Id { get; set; }
+    public required string UserName {get; set;}
+}
---
+++ API/Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapControllers();
+
+app.Run();

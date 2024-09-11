12aba585acbcdc40b5f19e1c88a536e3127474b0
CarterLeeW <103856985+CarterLeeW@users.noreply.github.com>
2024-09-11 10:02:55 -0400

    register component

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  model: any = {};
+
+  register() {
+    console.log(this.model);
+  }
+
+  cancel() {
+    console.log('cancelled');
+  }
+}

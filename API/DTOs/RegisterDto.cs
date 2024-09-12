7dc35531601cd0f76c875d33b994c5f1ce4e9222
CarterLeeW <103856985+CarterLeeW@users.noreply.github.com>
2024-09-12 13:44:44 -0400

    server side error handling

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -6,7 +6,8 @@ namespace API.DTOs;
 public class RegisterDto
 {
     [Required]
-    public required string Username { get; set; }
+    public string Username { get; set; } = string.Empty;
     [Required]
-    public required string Password { get; set; }
+    [StringLength(8, MinimumLength =4)]
+    public string Password { get; set; } = string.Empty;
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,5 @@
-using System.Text;
-using API.Data;
 using API.Extensions;
-using API.Interfaces;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +12,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 // Middleware goes here
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(
     x => x.AllowAnyHeader()
     .AllowAnyMethod()

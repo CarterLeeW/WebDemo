b32fd06e786b4001d0fb81d2e4bc429b1b421455
CarterLeeW <103856985+CarterLeeW@users.noreply.github.com>
2024-09-18 13:19:16 -0400

    photoservice

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,18 +1,21 @@
-using System.Reflection.Metadata.Ecma335;
 using System.Security.Claims;
-using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using API.Interfaces;
+using API.Services;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 [Authorize]
-public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
+public class UsersController(
+    IUserRepository userRepository, 
+    IMapper mapper, 
+    IPhotoService photoService) 
+    : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
@@ -38,12 +41,7 @@ public class UsersController(IUserRepository userRepository, IMapper mapper) : B
     [HttpPut]
     public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
     {
-        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-        if (username== null) {
-            return BadRequest("No username found in token");
-        }
-
-        var user = await userRepository.GetUserByUsernameAsync(username);
+        var user = await userRepository.GetUserByUsernameAsync(User.GetUsername());
         if (user == null) {
             return BadRequest("Could not find user");
         }
@@ -53,5 +51,37 @@ public class UsersController(IUserRepository userRepository, IMapper mapper) : B
             return NoContent();
         }
         return BadRequest("Failed to updte the user");
+    }
+
+    [HttpPost("add-photo")]
+    public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
+    {
+        var user = await userRepository.GetUserByUsernameAsync(User.GetUsername());
+
+        if (user == null)
+        {
+            return BadRequest("Cannot update user");
+        }
+
+        var result = await photoService.AddPhotoAsync(file);
+
+        if (result.Error != null)
+        {
+            return BadRequest(result.Error.Message);
+        }
+
+        var photo = new Photo
+        {
+            Url = result.SecureUrl.AbsoluteUri,
+            PublicId = result.PublicId
+        };
+
+        user.Photos.Add(photo);
+
+        if (await userRepository.SaveAllAsync())
+        {
+            return mapper.Map<PhotoDto>(photo);
+        }
+        return BadRequest("Problem adding photo");
     }
 }
---
+++ API/Interfaces/IPhotoService.cs
@@ -0,0 +1,11 @@
+using System;
+using CloudinaryDotNet.Actions;
+
+namespace API.Interfaces;
+
+public interface IPhotoService
+{
+    Task<ImageUploadResult> AddPhotoAsync(IFormFile file);
+    Task<DeletionResult> DeletePhotoAsync(string publicId);
+}
+

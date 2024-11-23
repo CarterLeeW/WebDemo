08da909e4f756664258a14e858c24d6dfb1d3bec
CarterLeeW <103856985+CarterLeeW@users.noreply.github.com>
2024-11-22 19:42:49 -0500

    starting signalr on client

---
+++ client/src/app/_services/presence.service.ts
@@ -0,0 +1,40 @@
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HubConnection, HubConnectionBuilder, HubConnectionState } from '@microsoft/signalr';
+import { ToastrService } from 'ngx-toastr';
+import { User } from '../_models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PresenceService {
+  hubUrl = environment.hubsUrl;
+  private hubConnection?: HubConnection;
+  private toastr = inject(ToastrService);
+
+  createHubConnection(user: User) {
+    this.hubConnection = new HubConnectionBuilder()
+      .withUrl(this.hubUrl + 'presence', {
+        accessTokenFactory: () => user.token
+      })
+      .withAutomaticReconnect()
+      .build();
+
+    this.hubConnection.start().catch(error => console.log(error));
+
+    // matches specified name on server
+    this.hubConnection.on('UserIsOnline', username => {
+      this.toastr.info(username + ' has connected');
+    });
+
+    this.hubConnection.on('UserIsOffline', username => {
+      this.toastr.warning(username + ' has disconnected');
+    });
+  }
+
+  stopHubConnection() {
+    if (this.hubConnection?.state === HubConnectionState.Connected) {
+      this.hubConnection.stop().catch(error => console.log(error));
+    }
+  }
+}
--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:5000/api/'
+    apiUrl: 'http://localhost:5000/api/',
+    hubsUrl: 'http://localhost:5000/hubs/',
+
 };
